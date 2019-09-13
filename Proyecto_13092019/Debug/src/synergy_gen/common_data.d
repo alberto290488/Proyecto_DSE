953018a682c69d250c5a00129b346450cce60cf2
alberto290488 <42017006+alberto290488@users.noreply.github.com>
2019-09-13 15:36:20 -0500

    00

---
+++ Proyecto_13092019/Debug/src/synergy_gen/common_data.d
@@ -0,0 +1,160 @@
+src/synergy_gen/common_data.o: ../src/synergy_gen/common_data.c \
+ ../src/synergy_gen/common_data.h \
+ E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy\ssp\inc\bsp/bsp_api.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\inc\ssp_common_api.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\inc\ssp_version.h \
+ E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy_cfg\ssp_cfg\bsp/bsp_cfg.h \
+ E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy_cfg\ssp_cfg\bsp/bsp_clock_cfg.h \
+ E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy_cfg\ssp_cfg\bsp/bsp_mcu_family_cfg.h \
+ E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_pn_cfg.h \
+ E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_cfg.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\s7g2\bsp_mcu_info.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\s7g2.h \
+ E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy\ssp\inc\bsp\cmsis\Include/core_cm4.h \
+ E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy\ssp\inc\bsp\cmsis\Include/cmsis_version.h \
+ E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy\ssp\inc\bsp\cmsis\Include/cmsis_compiler.h \
+ E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy\ssp\inc\bsp\cmsis\Include/cmsis_gcc.h \
+ E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy\ssp\inc\bsp\cmsis\Include/mpu_armv7.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\system_S7G2.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\s7g2\bsp_elc.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\s7g2\bsp_cache.h \
+ E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy_cfg\ssp_cfg\bsp/bsp_board_cfg.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\board\s7g2_sk\bsp.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\board\s7g2_sk\bsp_init.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\board\s7g2_sk\bsp_leds.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\board\s7g2_sk\bsp_qspi.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\board\s7g2_sk\bsp_ethernet_phy.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\all\bsp_common.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\inc\ssp_common_api.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\all\bsp_register_protection.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\all\bsp_locking.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\all\bsp_irq.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\all\bsp_group_irq.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\all\bsp_clocks.h \
+ E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy\ssp\inc\driver\api/r_ioport_api.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\inc\ssp_features.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\inc\ssp_common_api.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h \
+ E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy\ssp\inc\driver\instances/r_fmi.h \
+ E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy\ssp\inc\driver\api/r_fmi_api.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\all\bsp_common_leds.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\all\bsp_delay.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\all\bsp_common.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\all\bsp_feature.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\all\bsp_mcu_api.h \
+ e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\all\bsp_error_checking.h \
+ E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy\ssp\inc\driver\instances/r_elc.h \
+ E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy_cfg\ssp_cfg\driver/r_elc_cfg.h \
+ E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy\ssp\inc\driver\api/r_elc_api.h \
+ E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy\ssp\inc\driver\instances/r_ioport.h \
+ E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy_cfg\ssp_cfg\driver/r_ioport_cfg.h \
+ E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy\ssp\inc\driver\instances/r_cgc.h \
+ E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy_cfg\ssp_cfg\driver/r_cgc_cfg.h \
+ E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy\ssp\inc\driver\api/r_cgc_api.h
+
+../src/synergy_gen/common_data.h:
+
+E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy\ssp\inc\bsp/bsp_api.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\inc\ssp_common_api.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\inc\ssp_version.h:
+
+E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy_cfg\ssp_cfg\bsp/bsp_cfg.h:
+
+E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy_cfg\ssp_cfg\bsp/bsp_clock_cfg.h:
+
+E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy_cfg\ssp_cfg\bsp/bsp_mcu_family_cfg.h:
+
+E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_pn_cfg.h:
+
+E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_cfg.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\s7g2\bsp_mcu_info.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\s7g2.h:
+
+E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy\ssp\inc\bsp\cmsis\Include/core_cm4.h:
+
+E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy\ssp\inc\bsp\cmsis\Include/cmsis_version.h:
+
+E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy\ssp\inc\bsp\cmsis\Include/cmsis_compiler.h:
+
+E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy\ssp\inc\bsp\cmsis\Include/cmsis_gcc.h:
+
+E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy\ssp\inc\bsp\cmsis\Include/mpu_armv7.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\system_S7G2.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\s7g2\bsp_elc.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\s7g2\bsp_cache.h:
+
+E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy_cfg\ssp_cfg\bsp/bsp_board_cfg.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\board\s7g2_sk\bsp.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\board\s7g2_sk\bsp_init.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\board\s7g2_sk\bsp_leds.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\board\s7g2_sk\bsp_qspi.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\board\s7g2_sk\bsp_ethernet_phy.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\all\bsp_common.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\inc\ssp_common_api.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\all\bsp_register_protection.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\all\bsp_locking.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\all\bsp_irq.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\all\bsp_group_irq.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\all\bsp_clocks.h:
+
+E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy\ssp\inc\driver\api/r_ioport_api.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\inc\ssp_features.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\inc\ssp_common_api.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h:
+
+E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy\ssp\inc\driver\instances/r_fmi.h:
+
+E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy\ssp\inc\driver\api/r_fmi_api.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\all\bsp_common_leds.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\all\bsp_delay.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\all\bsp_common.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\all\bsp_feature.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\all\bsp_mcu_api.h:
+
+e:\toshiba_particion\biblioteca\uteq\diplomado\proyecto\proyecto_13092019\synergy\ssp\src\bsp\mcu\all\bsp_error_checking.h:
+
+E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy\ssp\inc\driver\instances/r_elc.h:
+
+E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy_cfg\ssp_cfg\driver/r_elc_cfg.h:
+
+E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy\ssp\inc\driver\api/r_elc_api.h:
+
+E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy\ssp\inc\driver\instances/r_ioport.h:
+
+E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy_cfg\ssp_cfg\driver/r_ioport_cfg.h:
+
+E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy\ssp\inc\driver\instances/r_cgc.h:
+
+E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy_cfg\ssp_cfg\driver/r_cgc_cfg.h:
+
+E:\Toshiba_Particion\Biblioteca\UTEQ\Diplomado\Proyecto\Proyecto_13092019\synergy\ssp\inc\driver\api/r_cgc_api.h:
